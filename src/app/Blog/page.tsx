7eb581fb5a4d0fdd17f92128fe211aaac59a3330
Monjurul Alam <monjurulalam190629@gmail.com>
2025-04-13 22:37:06 +0600

    image optimization is now ok

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
+import Image from 'next/image'
+import img from '../../../public/globe.svg';
 
 const page: React.FC = async () => {
     const data = await fetch('https://api.vercel.app/blog');
@@ -10,9 +12,12 @@ const page: React.FC = async () => {
             Blog : {posts.length}
             {
                 newPosts.map(post => <li key={post.id}>
-                    <Link href ={`/Blog/${post.id}`}>{post.title}</Link>
+                    <Link href={`/Blog/${post.id}`}>{post.title}</Link>
                 </li>)
             }
+            <div className='w-[450px] my-10'>
+                <Image className='w-[250px]' src={img} alt="window image" />
+            </div>
         </div>
     );
 };
