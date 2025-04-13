400015890e7421ea95d906a6030bd43bb375b2fb
Monjurul Alam <monjurulalam190629@gmail.com>
2025-04-14 00:19:48 +0600

    revalidate is understand now

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -4,8 +4,12 @@ import Image from 'next/image'
 import img from '../../../public/globe.svg';
 
 const page: React.FC = async () => {
-    const data = await fetch('https://api.vercel.app/blog');
+    const data = await fetch('https://api.vercel.app/blog',{
+        next: { revalidate: 20 }, // Revalidate every 60 seconds
+      });
     const posts = await data.json();
+
+    
     const newPosts = posts.slice(0, 5);
     return (
         <div>
--- src/app/lib/getPostById.ts
+++ src/app/lib/getPostById.ts
@@ -1,7 +1,9 @@
 
 
+
 export default async function getPostById(id){
     const result = await fetch(`https://api.vercel.app/blog/${id}`);
+    
     if(!result.ok){
         throw new Error("there was a fetching error")
     }
