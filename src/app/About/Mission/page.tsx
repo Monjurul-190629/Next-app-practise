3aed65c59c283219e487c6d9ee9faa5fcb640863
Monjurul Alam <monjurulalam190629@gmail.com>
2025-04-13 16:57:46 +0600

    ërror, not-found, loading are now added

--- src/app/About/Mission/page.tsx
+++ src/app/About/Mission/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const page = () => {
+    throw new Error('There is a huge error')
     return (
         <div>
             This is Mission page
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notFound = () => {
+    return (
+        <div>
+            Sorry, No resource in this section....
+        </div>
+    );
+};
+
+export default notFound;
